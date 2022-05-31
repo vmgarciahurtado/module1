a9e4cd884169f292f2825338d309c6b29b9c6def
vamgarcia <victor.garcia@celuweb.com>
2022-05-31 09:46:15 -0500

    cambios

--- lib/prueb_vm.dart
+++ lib/prueb_vm.dart
@@ -1,3 +1,7 @@
 import 'package:get/get.dart';
 
-class PruebaVm extends GetxController {}
+class PruebaVm extends GetxController {
+  PruebaVm() {
+    print('Funciona vm sin GetMaterialApp');
+  }
+}
